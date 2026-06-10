8a2a378f0b5d810af8c8e6f1fa72fe915a59ccf9
katleo-rantle <kprantle@gmail.com>
2026-06-10 15:44:50 +0200

    chore: initialize full-stack monorepo and book-network-api module configuration

---
+++ book-network-api/src/main/java/com/katleo/book/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.katleo.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
